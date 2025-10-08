f6b156b9e4fe993c40bf8303c07660d05c2f9bc1
Sammy Njue <sammynjue10@gmail.com>
2025-10-08 08:26:33 +0300

    Initial commit

---
+++ src/main/java/com/api/edusys/EdusysApplication.java
@@ -0,0 +1,13 @@
+package com.api.edusys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/edusys/EdusysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.edusys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
