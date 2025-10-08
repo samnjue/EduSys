ae15bbc1ef250f68b810c2f15056ba4b4269953a
Sammy Njue <sammynjue10@gmail.com>
2025-10-08 10:21:20 +0300

    Commit no. 2 - Entities & Student class

---
+++ src/main/java/com/api/edusys/entities/Student.java
@@ -0,0 +1,26 @@
+package main.java.com.api.edusys.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GenerateValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private int age;
+}
+
