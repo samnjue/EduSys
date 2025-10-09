db549323c751fe7a6cb8ee8223e5f58587e34d0f
Sammy Njue <sammynjue10@gmail.com>
2025-10-09 11:41:47 +0300

    Commit no. 9 - Teacher DTO

---
+++ src/main/java/com/api/edusys/dto/TeacherDTO.java
@@ -0,0 +1,27 @@
+package com.api.edusys.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherDTO {
+    
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be of valid format")
+    private String email;
+
+    @NotBlank(message = "Department is required")
+    private String department;
+}
